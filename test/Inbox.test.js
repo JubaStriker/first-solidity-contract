06c4c68dc37c1a17736fe3446f0d2f25e02d70f8
JubaStriker <jubastriker@gmail.com>
2023-08-25 12:53:34 +0600

    testing code initialized

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -8,18 +8,53 @@ const assert = require('assert');
 const web3 = new Web3(ganache.provider());
 
 
-class Car {
-    park() {
-        return 'stopped';
-    }
-    drive() {
-        return 'vroom';
-    }
-}
 
-describe('Car class testing', () => {
-    it('park should return a string', () => {
-        const car = new Car();
-        assert.equal(car.park(), 'stopped');
-    })
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Car {
+//     park() {
+//         return 'stopped';
+//     }
+//     drive() {
+//         return 'vroom';
+//     }
+// }
+
+// let car;
+
+// beforeEach(() => {
+//     car = new Car();
+// })
+
+// describe('Car class testing', () => {
+//     it('park should return a string', () => {
+//         assert.equal(car.park(), 'stopped');
+//     })
+
+//     it('drive should return a string vroom', () => {
+//         assert.equal(car.drive(), 'vroom');
+//     })
+// })
