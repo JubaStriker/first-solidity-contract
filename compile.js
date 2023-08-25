a8b875b71049761a701a8ead3a45e3c6311c16f7
JubaStriker <jubastriker@gmail.com>
2023-08-25 12:35:10 +0600

    Testing with mocha

--- compile.js
+++ compile.js
@@ -1,1 +1,10 @@
 // compile code will go here
+
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol'); // Get the file path for the inbox file
+const source = fs.readFileSync(inboxPath, 'utf8'); // Read the source file of the specific path
+
+module.exports = solc.compile(source, 1).contracts[':Inbox'];
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,6 +1,25 @@
 const ganache = require('ganache');
 const { Web3 } = require('web3');
+const assert = require('assert');
 
 // updated ganache and web3 imports added for convenience
 
 // contract test code will go here
+const web3 = new Web3(ganache.provider());
+
+
+class Car {
+    park() {
+        return 'stopped';
+    }
+    drive() {
+        return 'vroom';
+    }
+}
+
+describe('Car class testing', () => {
+    it('park should return a string', () => {
+        const car = new Car();
+        assert.equal(car.park(), 'stopped');
+    })
+})
