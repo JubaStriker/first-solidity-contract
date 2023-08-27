ca0f8a9eb94e09322c06a466f714d1345aad83d2
JubaStriker <jubastriker@gmail.com>
2023-08-27 23:15:22 +0600

    First contract deployed

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,6 +1,7 @@
 const ganache = require('ganache');
 const { Web3 } = require('web3');
 const assert = require('assert');
+const { interface, bytecode } = require('../compile');
 
 // updated ganache and web3 imports added for convenience
 
@@ -8,16 +9,20 @@ const assert = require('assert');
 const web3 = new Web3(ganache.provider());
 
 let accounts;
+let inbox;
 
 beforeEach(async () => {
     // Get the list of all accounts
     // Use one of those accounts to deploy the contract
     accounts = await web3.eth.getAccounts()
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there'] })
+        .send({ from: accounts[0], gas: '1000000' })
 })
 
 describe("Inbox", () => {
     it('Deploys a contract', () => {
-        console.log(accounts)
+        console.log(inbox)
     })
 })
 
