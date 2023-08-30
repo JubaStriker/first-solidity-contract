4e89c671c5d1981dc328faeb0488aac4f8bc5e39
JubaStriker <jubastriker@gmail.com>
2023-08-30 23:47:03 +0600

    Lottery added

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,16 +1,35 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.0;
 
-contract Lottery {
+contract Lotter {
     address public manager;
-    address[] public players;
+    address payable[] public players;
+    address payable[] payPlayers;
 
-    function lottery() public {
+    function Lottery() public {
         // msg is a global object that holds the address that invocated the current function in msg.sender
         manager = msg.sender;
     }
 
     function enter() public payable {
+        require(msg.value > 0.001 ether);
         players.push(msg.sender);
+    }
+
+    function random() private view returns (uint) {
+        return
+            uint(
+                keccak256(
+                    abi.encodePacked(block.prevrandao, block.timestamp, players)
+                )
+            );
+        // Difficulty is replaced by prevrandao
+    }
+
+    function pickWinner() public {
+        uint index = random() % players.length;
+        //players[index] will be a type of address that is like objects that can have methods
+
+        players[index].transfer(address(this).balance);
     }
 }
